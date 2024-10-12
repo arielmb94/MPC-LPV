06f6d01373a4094fd7710dae5ae2974a4cc9c4fe
Ariel Medero <arielmedero@mbp-de-ariel.home>
2024-10-12 15:32:29 +0200

    Handle empty Dd or d in get_y()

--- get_y.m
+++ get_y.m
@@ -1,14 +1,29 @@
 function y = get_y(s,u,d,nx,nu,ny,nd,N,Ny,C,D,Dd)
 
-    y = zeros(Ny,1);
-    for k = 1:N
+    if isempty(d) || isempty(Dd)
 
-        sk = s((k-1)*nx+1:k*nx);
-        uk = u((k-1)*nu+1:k*nu);
-        %dk = d((k-1)*nd+1:k*nd)
-
-        y((k-1)*ny+1:k*ny) = C*sk+D*uk;
+        y = zeros(Ny,1);
+        for k = 1:N
     
-    end
+            sk = s((k-1)*nx+1:k*nx);
+            uk = u((k-1)*nu+1:k*nu);
+    
+            y((k-1)*ny+1:k*ny) = C*sk+D*uk;
         
+        end
+    
+    else
+
+        y = zeros(Ny,1);
+        for k = 1:N
+    
+            sk = s((k-1)*nx+1:k*nx);
+            uk = u((k-1)*nu+1:k*nu);
+            dk = d((k-1)*nd+1:k*nd);
+    
+            y((k-1)*ny+1:k*ny) = C*sk+D*uk+Dd*dk;
+        
+        end
+        
+    end
 end
