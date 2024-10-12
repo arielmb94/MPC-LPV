4346951da3b28697e441f3aed973c5ad06be9795
Ariel Medero <arielmedero@mbp-de-ariel.home>
2024-10-12 15:30:30 +0200

    Handle empty Bd or d in update_beq()

--- update_beq.m
+++ update_beq.m
@@ -1,14 +1,21 @@
 function beq = update_beq(beq,A,x_prev,N,nx,Bd,d)
 
-for k = 1:N+1
+    if isempty(d) || isempty(Bd)
 
-    switch k 
-        case 1
-            beq((k-1)*nx+1:k*nx) = -A*x_prev-Bd*d(k);            
-        otherwise
+        beq(1:nx) = -A*x_prev;
 
-            beq((k-1)*nx+1:k*nx) = -Bd*d(k);
+    else
+
+        for k = 1:N+1
+        
+            switch k 
+                case 1
+                    beq((k-1)*nx+1:k*nx) = -A*x_prev-Bd*d(k);            
+                otherwise
+        
+                    beq((k-1)*nx+1:k*nx) = -Bd*d(k);
+            end
+        end
     end
-end
 
 end
