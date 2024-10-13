1eea88ad3242a64d65e8c6ab5b9f7d92e6171623
Ariel Medero <arielmedero@mbp-de-ariel.home>
2024-10-13 18:20:48 +0200

    Add missing required function inputs; add empty value for not computed box constraints

--- get_state_constraint_info.m
+++ get_state_constraint_info.m
@@ -2,7 +2,7 @@ function [s,s_ter,u,du,y,err,...
     fi_s_min_x0,fi_s_max_x0,fi_s_ter_min_x0,fi_s_ter_max_x0,...
     fi_u_min_x0,fi_u_max_x0,fi_du_min_x0,fi_du_max_x0,...
     fi_y_min_x0,fi_y_max_x0,feas] = ...
-    get_state_constraint_info(x,mpc,check_feas)
+    get_state_constraint_info(x,u_prev,r,d,mpc,check_feas)
 
 %states
 [s,s_ter] = get_x(x,mpc.nx,mpc.nu,mpc.N,mpc.Nx);
@@ -23,14 +23,20 @@ if ~isempty(mpc.x_min) && ~isempty(mpc.x_max)
     if check_feas && ( any(fi_s_min_x0>0) || any(fi_s_max_x0>0) )
         feas = 0;
     end
+else
+    fi_s_min_x0 = []; 
+    fi_s_max_x0 = [];
 end
 
 % Terminal State box constraints
-if feas && ~isempty(mpc.x_ter_min) && ~isempty(mpc.x_ter_max) 
+if feas && ~isempty(mpc.x_ter_min) && ~isempty(mpc.x_ter_max)
     [fi_s_ter_min_x0,fi_s_ter_max_x0] = fi_box_fun(s_ter,mpc.x_ter_min,mpc.x_ter_max,mpc.nx,mpc.nx);
     if check_feas && ( any(fi_s_ter_min_x0>0) || any(fi_s_ter_max_x0>0) )
         feas = 0;
     end
+else
+    fi_s_ter_min_x0 = [];
+    fi_s_ter_max_x0 = [];
 end
 
 % Control box constraints
@@ -39,6 +45,9 @@ if feas && ~isempty(mpc.u_min) && ~isempty(mpc.u_max)
     if check_feas && ( any(fi_u_min_x0>0) || any(fi_u_max_x0>0) )
         feas = 0;
     end
+else
+    fi_u_min_x0 = [];
+    fi_u_max_x0 = [];
 end
 
 % Differential Control box constraints
@@ -47,6 +56,9 @@ if feas && ~isempty(mpc.du_min) && ~isempty(mpc.du_max)
     if check_feas && ( any(fi_du_min_x0>0) || any(fi_du_max_x0>0) )
         feas = 0;
     end
+else
+    fi_du_min_x0 = [];
+    fi_du_max_x0 = [];
 end
 
 % Outputs box constraints
@@ -55,6 +67,9 @@ if feas && ~isempty(mpc.y_min) && ~isempty(mpc.y_max)
     if check_feas && ( any(fi_y_min_x0>0) || any(fi_y_max_x0>0) )
         feas = 0;
     end
+else
+    fi_y_min_x0 = [];
+    fi_y_max_x0 = [];
 end
 
 end
