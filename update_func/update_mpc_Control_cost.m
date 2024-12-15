cce284e95050ba2d94dff9c2ba6a5a3eb54617b7
Ariel Medero <arielmedero@MacBook-Pro-de-Ariel.local>
2024-12-15 12:36:46 +0100

    update functions for control and diff. control costs

--- update_func/update_mpc_Control_cost.m
+++ update_func/update_mpc_Control_cost.m
@@ -1,14 +1,10 @@
-function mpc = set_mpc_Control_cost(mpc,Ru)
+function mpc = update_mpc_Control_cost(mpc,Ru)
 
 mpc.Ru = Ru;
-
-if isempty(mpc.hessCost)
-    mpc.hessCost = zeros(mpc.Nu+mpc.Nx);
-end
 
 [mpc.gradCtlrRu,mpc.hessCtrlTerm] = genControlGradHess(Ru,mpc.N,...
     mpc.Nx,mpc.Nu,mpc.nx,mpc.nu);
 
-mpc.hessCost = mpc.hessCost + mpc.hessCtrlTerm;
+mpc.recompute_cost_hess = 1;
 
 end
--- update_func/update_mpc_DiffControl_cost.m
+++ update_func/update_mpc_DiffControl_cost.m
@@ -1,14 +1,10 @@
-function mpc = set_mpc_DiffControl_cost(mpc,Rdu)
+function mpc = update_mpc_DiffControl_cost(mpc,Rdu)
 
 mpc.Rdu = Rdu;
-
-if isempty(mpc.hessCost)
-    mpc.hessCost = zeros(mpc.Nu+mpc.Nx);
-end
 
 [mpc.gradDiffCtlrRdu,mpc.hessDiffCtrlTerm] = genDiffControlGradHess(Rdu,mpc.N,...
     mpc.Nx,mpc.Nu,mpc.nx,mpc.nu);
 
-mpc.hessCost = mpc.hessCost + mpc.hessDiffCtrlTerm;
+mpc.recompute_cost_hess = 1;
 
 end
