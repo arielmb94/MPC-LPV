78f310bad1b9caa4bae3d8b26678f5d0302e16ed
Ariel Medero <arielmedero@MacBook-Pro-de-Ariel.local>
2024-12-17 09:59:33 +0100

    update function for output equation and tracking cost

--- update_func/update_mpc_Tracking_cost.m
+++ update_func/update_mpc_Tracking_cost.m
@@ -1,14 +1,12 @@
-function mpc = set_mpc_Tracking_cost(mpc,Qe)
+function mpc = update_mpc_Tracking_cost(mpc,Qe)
 
-mpc.Qe = Qe;
+mpc.recompute_cost_hess = 1;
 
-if isempty(mpc.hessCost)
-    mpc.hessCost = zeros(mpc.Nu+mpc.Nx);
+if ~isempty(Qe)   
+    mpc.Qe = Qe;   
 end
 
-[mpc.gradErrQe,mpc.hessErrTerm] = genLinOutGradHess(Qe,mpc.C,mpc.D,mpc.N,...
-        mpc.Nx,mpc.Nu,mpc.Ny,mpc.nx,mpc.nu,mpc.ny);
-
-mpc.hessCost = mpc.hessCost + mpc.hessErrTerm;
+[mpc.gradErrQe,mpc.hessErrTerm] = genLinOutGradHess(mpc.Qe,mpc.C,mpc.D,...
+            mpc.N,mpc.Nx,mpc.Nu,mpc.Ny,mpc.nx,mpc.nu,mpc.ny);
 
 end
