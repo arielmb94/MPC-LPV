58960405c02a902bec5928cdde66e6866e837ae9
Ariel Medero <arielmedero@MacBook-Pro-de-Ariel.local>
2024-12-23 16:40:03 +0100

    addapt state and terminal state to reduced control horizon

--- genGradXter.m
+++ genGradXter.m
@@ -1,4 +1,4 @@
-function [gradXtermin,gradXtermax] = genGradXter(N,Nx,Nu,nx,nu)
+function [gradXtermin,gradXtermax] = genGradXter(N,N_h_ctr,Nx,Nu,nx,nu)
 %optimization variables are stored as [u0 x1 u1 x2 ... xn un xn+1]^T
 
 % x_min - xk <= 0 
@@ -7,7 +7,15 @@ function [gradXtermin,gradXtermax] = genGradXter(N,Nx,Nu,nx,nu)
 gradXtermax = zeros(Nx+Nu,nx);
 k = N;
 
-gradXtermax(nu + nx*(k-1)+ nu*(k-1) + 1 : nu + nx*(k)+ nu*(k-1), :) = eye(nx);
+if k < N_h_ctr
+    gradXtermax(nu + nx*(k-1)+ nu*(k-1) + 1 : nu + nx*(k)+ nu*(k-1), : ) = ...
+        eye(nx);
+else
+    gradXtermax(nu + (nx+nu)*(N_h_ctr-1) + nx*(k-N_h_ctr) + 1  : ...
+        nu + (nx+nu)*(N_h_ctr-1) + nx*(k-N_h_ctr) + nx, : ) = eye(nx);
+end
+
+
 gradXtermin = -gradXtermax;
 
 end
