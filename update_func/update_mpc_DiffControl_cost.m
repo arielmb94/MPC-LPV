e4c242380020a13494eda6156e46dc715ad8c7e1
Ariel Medero <arielmedero@MacBook-Pro-de-Ariel.local>
2024-12-26 16:15:29 +0100

    adapt update functions for reduced control horizon

--- update_func/update_mpc_DiffControl_cost.m
+++ update_func/update_mpc_DiffControl_cost.m
@@ -2,8 +2,8 @@ function mpc = update_mpc_DiffControl_cost(mpc,Rdu)
 
 mpc.Rdu = Rdu;
 
-[mpc.gradDiffCtlrRdu,mpc.hessDiffCtrlTerm] = genDiffControlGradHess(Rdu,mpc.N,...
-    mpc.Nx,mpc.Nu,mpc.nx,mpc.nu);
+[mpc.gradDiffCtlrRdu,mpc.hessDiffCtrlTerm] = genDiffControlGradHess(Rdu,...
+    mpc.N_ctr_hor,mpc.Nx,mpc.Nu,mpc.nx,mpc.nu);
 
 mpc.recompute_cost_hess = 1;
 
