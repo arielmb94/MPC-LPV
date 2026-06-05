beb7006f2278d8ebce2c30dd616a0c0cdaff1a39
Ariel Medero <arielmedero@MacBook-Pro-de-Ariel.local>
2026-06-05 12:46:15 +0200

    adapt initial state rolling

--- mpc_definition_api/init_func/build_chronos_mpc.m
+++ mpc_definition_api/init_func/build_chronos_mpc.m
@@ -30,14 +30,13 @@
 %
 %   OUTPUTS:
 %       x0     - Primal vector [u_0; x_1; u_1; ... x_Nc; ... x_N; v] 
-function [mpc,x0] = build_chronos_mpc(mpc,s_prev,u_prev,x_ref,d_in,dh_in)
+function mpc = build_chronos_mpc(mpc,s_prev,u_prev,d_in,x_ref)
 arguments
     mpc
     s_prev
     u_prev
-    x_ref = []
     d_in = []
-    dh_in = []
+    x_ref = []
 end
 
     % get index maps for optimization variables
@@ -53,154 +52,101 @@ end
     mpc = init_costs(mpc);
 
     % compute primal variables vector
-    x0 = rollstates(mpc,s_prev,u_prev,x_ref,d_in);
-
-    % set x_ref to s_prev if user does not pass x_ref but there are
-    % terminal constraints
-%    if isempty(x_ref) && mpc.ter_constraint
-%        x_ref = s_prev;
-%    end
-
-    % handle disturbance input vectors size
     if ~isempty(d_in) && length(d_in)< mpc.Nd
         d = fill_vec(d_in,mpc.nd,mpc.Nd,1);
+        d = reshape(d,mpc.nd,mpc.N);
     else
         d = d_in;
-    end
-    
-    if ~isempty(dh_in) && length(dh_in)< mpc.Nd
-        dh = fill_vec(dh_in,mpc.ndh,mpc.Ndh,1);
-    else
-        dh = dh_in;
-    end
-    
-    
+    end 
 
+    x0 = rollstates(mpc,s_prev,u_prev,x_ref,d);
+
+    x0(mpc.slack_index) = 1/mpc.t;
+    mpc.x0 = x0;
+    
 end
 
 function x0 = rollstates(mpc,s_prev,u_prev,x_ref,d_in)
 
-    x0 = zeros(mpc.Nx+mpc.Nu+mpc.Nv,1);
-    
-    x_k = s_prev;
-    u_k_prev = u_prev;
-    
-    for k = 1 : mpc.N_ctr_hor
-        % Compute Raw Control Input
-        if ~isempty(mpc.K) && ~isempty(x_ref)
-            % Option 2: Terminal ingredients exist and user pass x_ref
-            u_raw = mpc.K * (x_ref - x_k);
-        else
-            % Option 1: Constant previous input
-            u_raw = u_k_prev;
-        end
-    
-        u_k = u_raw;
-    
-        % 2. Clip for Rate Constraints (Delta u)
-        if ~isempty(mpc.du_cnstr)
-            % Check minimum rate limit
-            if mpc.du_cnstr.min_limit
-                du_min_strict = mpc.du_cnstr.min + mpc.slack_epsilon;
-                u_k = max(u_k_prev + du_min_strict, u_k);
-            end
-            % Check maximum rate limit
-            if mpc.du_cnstr.max_limit
-                du_max_strict = mpc.du_cnstr.max - mpc.slack_epsilon;
-                u_k = min(u_k_prev + du_max_strict, u_k);
-            end
-        end
-    
-        % 3. Clip for Absolute Constraints (u)
-        if ~isempty(mpc.u_cnstr)
-            % Check minimum absolute limit
-            if mpc.u_cnstr.min_limit
-                u_min_strict = mpc.u_cnstr.min + mpc.slack_epsilon;
-                u_k = max(u_min_strict, u_k);
-            end
-            % Check maximum absolute limit
-            if mpc.u_cnstr.max_limit
-                u_max_strict = mpc.u_cnstr.max - mpc.slack_epsilon;
-                u_k = min(u_max_strict, u_k);
-            end
-        end
-    
-        % 4. Propagate Dynamics
-        % x_{k+1} = A*x_k + B*u_k + D*d_k
-        x_next = mpc.A * x_k + mpc.B * u_k;
-        if ~isempty(mpc.Bd) && ~isempty(d_in)
-            x_next = x_next + mpc.Bd * d_in;
-        end
-        % clamp x: for safety net in case we are dealing with unstable
-        % system
-        if ~isempty(mpc.s_cnstr)
-            % Check minimum state limits
-            if mpc.s_cnstr.min_limit
-                s_min_strict = mpc.s_cnstr.min + mpc.slack_epsilon;
-                x_next = max(s_min_strict, x_next);
-            end
-    
-            % Check maximum state limits
-            if mpc.s_cnstr.max_limit
-                s_max_strict = mpc.s_cnstr.max - mpc.slack_epsilon;
-                x_next = min(s_max_strict, x_next);
-    
-            end
-        end
-    
-        % 5. Map to Primal Optimization Vector
-        % Indexing math for [u0; x1; u1; x2; ...]
-        idx_start = (k - 1) * (mpc.nu + mpc.nx);
-    
-        % Insert u_k
-        x0(idx_start + 1 : idx_start + mpc.nu) = u_k;
-    
-        % Insert x_{next}
-        x0(idx_start + mpc.nu + 1 : idx_start + mpc.nu + mpc.nx) = x_next;
-    
-        % 6. Prepare for next step
-        x_k = x_next;
-        u_k_prev = u_k;
+x0 = zeros(mpc.n,1);
+
+x_k = s_prev;
+u_k_prev = u_prev;
+
+for k = 1 : mpc.N
+    % Compute Raw Control Input
+    if ~isempty(mpc.K) && ~isempty(x_ref)
+        % Option 2: Terminal ingredients exist and user pass x_ref
+        u_raw = mpc.K * (x_ref - x_k);
+    else
+        % Option 1: Constant previous input
+        u_raw = u_k_prev;
     end
-    
-    if mpc.N_ctr_hor < mpc.N
-        u_N_ctr_hor = u_k;
-        idx_start_N_ctr_hor = (mpc.N_ctr_hor-1)*(mpc.nu + mpc.nx) + mpc.nu;
 
-        for k = mpc.N_ctr_hor + 1 : mpc.N
-            % 4. Propagate Dynamics
-            % x_{k+1} = A*x_k + B*u_k + D*d_k
-            x_next = mpc.A * x_k + mpc.B * u_N_ctr_hor;
-            if ~isempty(mpc.Bd) && ~isempty(d_in)
-                x_next = x_next + mpc.Bd * d_in;
-            end
-            % clamp x 
-            if ~isempty(mpc.s_cnstr)
-                % Check minimum state limits
-                if mpc.s_cnstr.min_limit
-    
-                    s_min_strict = mpc.s_cnstr.min + mpc.slack_epsilon;
-                    x_next = max(s_min_strict, x_next);
-                end
-                % Check maximum state limits
-                if mpc.s_cnstr.max_limit
+    u_k = u_raw;
 
-                    s_max_strict = mpc.s_cnstr.max - mpc.slack_epsilon;
-                    x_next = min(s_max_strict, x_next);
-                end
-            end
-
-            % 5. Map to Primal Optimization Vector
-            % Indexing math for [u0; x1; u1; x2; ...]
-            idx_start = idx_start_N_ctr_hor + (k - mpc.N_ctr_hor) * mpc.nx;
-
-            % Insert x_{next}
-            x0(idx_start + 1 : idx_start + mpc.nx) = x_next;
-
-            % 6. Prepare for next step
-            x_k = x_next;
-
-        end  
+    % 2. Clip for Rate Constraints (Delta u)
+    if mpc.has_du_cnstr
+        % Check minimum rate limit
+        if mpc.du_cnstr.min_limit
+            du_min_strict = mpc.du_cnstr.min + mpc.slack_epsilon;
+            u_k = max(u_k_prev + du_min_strict, u_k);
+        end
+        % Check maximum rate limit
+        if mpc.du_cnstr.max_limit
+            du_max_strict = mpc.du_cnstr.max - mpc.slack_epsilon;
+            u_k = min(u_k_prev + du_max_strict, u_k);
+        end
     end
+
+    % 3. Clip for Absolute Constraints (u)
+    if mpc.has_u_cnstr
+        % Check minimum absolute limit
+        if mpc.u_cnstr.min_limit
+            u_min_strict = mpc.u_cnstr.min + mpc.slack_epsilon;
+            u_k = max(u_min_strict, u_k);
+        end
+        % Check maximum absolute limit
+        if mpc.u_cnstr.max_limit
+            u_max_strict = mpc.u_cnstr.max - mpc.slack_epsilon;
+            u_k = min(u_max_strict, u_k);
+        end
+    end
+
+    % 4. Propagate Dynamics
+    % x_{k+1} = A*x_k + B*u_k + D*d_k
+    x_next = mpc.A * x_k + mpc.B * u_k;
+    if ~isempty(mpc.Bd) && ~isempty(d_in)
+        x_next = x_next + mpc.Bd * d_in(:,k);
+    end
+    % clamp x: for safety net in case we are dealing with unstable
+    % system
+    if mpc.has_s_cnstr
+        % Check minimum state limits
+        if mpc.s_cnstr.min_limit
+            s_min_strict = mpc.s_cnstr.min + mpc.slack_epsilon;
+            x_next = max(s_min_strict, x_next);
+        end
+
+        % Check maximum state limits
+        if mpc.s_cnstr.max_limit
+            s_max_strict = mpc.s_cnstr.max - mpc.slack_epsilon;
+            x_next = min(s_max_strict, x_next);
+
+        end
+    end
+
+    % 5. Map to Primal Optimization Vector
+    % Indexing math for [u0; x1; u1; x2; ...]
+    x0(mpc.u_index_k(:,k)) = u_k;
+    if mpc.has_du && any(mpc.su_index_k(:,k))
+        x0(mpc.su_index_k(:,k)) = u_k_prev;
+    end
+    x0(mpc.s_index_k(:,k+1)) = x_next;
+
+    % 6. Prepare for next step
+    x_k = x_next;
+    u_k_prev = u_k;
+end
 
 end
